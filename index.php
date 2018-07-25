39d81f65dabc5713fe3a7a5475825668a7891678
Will S <will.sparks@swsparks.com>
2018-07-24 20:25:28 -0500

    php guessing game
    php guessing game

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Steven Sparks PHP</title>
+</head>
+<body>
+<h1>Steven Sparks PHP</h1>
+<pre>
+     _____
+    /     \
+    |
+    \____
+          \
+          |
+    \_____/
+</pre>
+<p>
+<?php
+print hash('sha256', 'Steven Sparks');
+?>
+</p>
+</body>
+</html>
